154ccdf4a2becec7f68971a73a2deb1b3d459885
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-14 10:50:04 -0300

    snippets engine and lua lsp

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -1,7 +1,6 @@
-local opts = { noremap=true, silent=true }
-local map = vim.api.nvim_buf_set_keymap 
+local map = vim.api.nvim_buf_set_keymap
 
-  -- Setup nvim-cmp.
+ -- Setup nvim-cmp.
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 local cmp = require'cmp'
 
@@ -33,28 +32,88 @@ cmp.setup({
   })
 
 
+local remapArgs = { noremap=true, silent=true }
+
 -- Setup lspconfig.
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
 		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = function()
-            map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-            map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-            map(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-            map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-            --map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-            map(bufnr, 'n', '<leader>]d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-            map(bufnr, 'n', '<leader>[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+            --map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            map({}, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', remapArgs)
+            map({}, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', remapArgs)
+            map({}, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', remapArgs)
+            map({}, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', remapArgs)
+            map({}, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', remapArgs)
+            map({}, 'n', '<leader>]d', '<cmd>lua vim.diagnostic.goto_next()<CR>', remapArgs)
+            map({}, 'n', '<leader>[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', remapArgs)
 
-            map(bufnr, 'n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-            map(bufnr, 'n', '<leader>co', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            map({}, 'n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<CR>', remapArgs)
+            map({}, 'n', '<leader>do', '<cmd>lua vim.lsp.buf.code_action()<CR>', remapArgs)
 		end,
 	}, _config or {})
 end
 
+
 require'lspconfig'.tsserver.setup( config() )
 require'lspconfig'.emmet_ls.setup( config( { filetypes = {"html", "css", "javascript", "javascriptreact", "typescriptreact"}}) )
-require'lspconfig'.eslint.setup{ config() }
---require'lspconfig'.tailwindcss.setup{ config() }
+require'lspconfig'.tailwindcss.setup{ config() }
 
+require'lspconfig'.eslint.setup{ config() }
 --vim.cmd('autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll')
+
+--sumneko lua lsp--------------------------------------------------------------------------------------------------------
+local sumneko_root_path = '/home/driven/personal/lua-language-server'
+local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup ( config( {
+    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+    settings = {
+        Lua = {
+          runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+          },
+          diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+          },
+          workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+          },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}) )
+--sumneko lua lsp--------------------------------------------------------------------------------------------------------
+
+--snippets
+local snippets_paths = function()
+	local plugins = { "friendly-snippets" }
+	local paths = {}
+	local path
+	local root_path = vim.env.HOME .. "/.vim/plugged/"
+	for _, plug in ipairs(plugins) do
+		path = root_path .. plug
+		if vim.fn.isdirectory(path) ~= 0 then
+			table.insert(paths, path)
+		end
+	end
+	return paths
+end
+
+require("luasnip.loaders.from_vscode").lazy_load({
+	paths = snippets_paths(),
+	include = nil, -- Load all languages
+	exclude = {},
+})
+--snippets
